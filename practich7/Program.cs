dcef8144e92f9844d509cca9093886a80e56f630
ИСП-22 <-22@LAB30-14>
2024-12-06 13:33:59 +0300

    Добавьте файлы проекта.

---
+++ practich7/Program.cs
@@ -0,0 +1,25 @@
+﻿int number, prev, proshloe;
+
+    Console.WriteLine("Введите последовательность чисел (окончить ввод числом 10000):");
+
+   
+    prev = int.Parse(Console.ReadLine());
+    proshloe = 1;
+
+
+    do
+    {
+        number = int.Parse(Console.ReadLine());
+        proshloe++;
+
+        if (number < prev)
+        {
+            Console.WriteLine($"Последовательность не упорядочена. Нарушение на позиции {proshloe}.");
+            return;
+        }
+
+        prev = number;
+
+    } while (number != 10000);
+
+    Console.WriteLine("Последовательность упорядочена по возрастанию.");
